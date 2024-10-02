3a96f1696a5085d90165b5f73d497e4ac87b7132
Savio <savioborges49@gmail.com>
2024-10-02 20:23:47 -0300

    contribuições do Sávio

--- _JS/script.js
+++ _JS/script.js
@@ -1,14 +1,9 @@
-//abre o HTML todo primeiro
 document.addEventListener('DOMContentLoaded', function () {
-   //pega os locais no HTMl
     const questoesBox = document.getElementById('questoes');
     const respostasBox = document.getElementById('respostas');
-    const contadorBox = document.getElementById('contador');
-    const pontosBox = document.getElementById('pontos');
-    //var que tem 3 objetos
+
     let perguntas = [
-        //fazendo instanciamento dos objetos
-        { questoes: "O que significa HTML?", respostas: ["HyperText Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language"], certo: 0 },//índice da resp porque resposta é um array
+        { questoes: "O que significa HTML?", respostas: ["HyperText Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language"], certo: 0 },
         { questoes: "Qual destas tecnologias é fundamental para o desenvolvimento de Inteligência Artificial (IA)?", respostas: ["Banco de Dados", "Aprendizado de Máquina (Machine Learning)", "Redes de Computadores", "Processadores Gráficos (GPU)"], certo: 1},
         { questoes: 'Em "Sword Art Online", a tecnologia VR (Realidade Virtual) é usada para criar mundos imersivos. Qual dessas tecnologias está mais próxima da VR atualmente?', respostas: ["Impressão 3D", "Realidade Aumentada (AR)", "Internet das Coisas (IoT)", "Computação em Nuvem"],certo: 1 },
         { questoes: "Qual linguagem de programação é amplamente usada para desenvolver engines de jogos como Unity?", respostas: ["Python", "C#", "JavaScript", "PHP"], certo: 1 },
@@ -21,63 +16,36 @@ document.addEventListener('DOMContentLoaded', function () {
         { questoes: 'No anime "Naruto", Shikamaru é conhecido por sua capacidade estratégica. Se ele trabalhasse com tecnologia, qual das seguintes técnicas seria mais útil para otimizar um algoritmo?', respostas: ["Bubble Sort", "Programação Dinâmica", "Pesquisa Linear", "Algoritmo Guloso (Greedy)"], certo: 1 }
     ];
 
-    //pega toas as perguntas (é uma var)
     let questoesDisponiveis = [...perguntas];
-    //para começar zerado
     let QuestaoAtual = 0;
-    let pontuacao = 0;
-    ContadorQuest()
 
     function CarregarProximaQuestao() {
-        //Se zerou as questões, já ganha
         if (questoesDisponiveis.length === 0) {
             window.location.href = 'vitoria.html';
             return;
         }
 
-        //var que recebe o método aleatório entre 0 e o num de quest
-        const randomizar = Math.floor(Math.random() * questoesDisponiveis.length); //num aleatório
+        const randomizar = Math.floor(Math.random() * questoesDisponiveis.length);
+        const questaoSelecionada = questoesDisponiveis.splice(randomizar, 1)[0];
 
-        //questões que já marcou e somem
-        const questaoSelecionada = questoesDisponiveis.splice(randomizar, 1)[0]; //splice tira uma string
-        //tira 1 do randomizar e pega o primeiro do array que o splice tira
-
-        //escrevendo no html a nova quest
         questoesBox.innerText = questaoSelecionada.questoes;
-        //limpando botões
         respostasBox.innerHTML = '';
 
-        //aparece a questão e resposta em botões
-        //pega a resposta dentro da var
-        //forEach cria um para cada, quantodade de índice vira a quant de botões
         questaoSelecionada.respostas.forEach((resposta, index) => {
-            const btn = document.createElement('button'); //botão
-            btn.innerText = resposta;//mostra a resp no botão
-            btn.onclick = () => VerificarRespostas(index, questaoSelecionada.certo); //vai chamar pra verificar toda vez que clicar no btn da resp e comparar com o certo
-            respostasBox.appendChild(btn); //mostra o botao na tela com o texto/resp já escrito
+            const btn = document.createElement('button');
+            btn.innerText = resposta;
+            btn.onclick = () => VerificarRespostas(index, questaoSelecionada.certo); 
+            respostasBox.appendChild(btn);
         });
-
-        //atualiza o contador
-        ContadorQuest()
     }
 
-    //verifica se está correto
-    //pega o índice da clicada e o correto lá em cima
     function VerificarRespostas(selecionado, correto) {
         if (selecionado === correto) {
-            CarregarProximaQuestao();//se tiver certo, gera outra aleatória 
-            QuestaoAtual++; //contador de questão 
-            pontuacao=pontuacao+20; //soma questão
-            ContadorQuest();
+            CarregarProximaQuestao();
         } else {
-            window.location.href = 'derrota.html';//errou
+            window.location.href = 'derrota.html';
         }
     }
 
-    function ContadorQuest(){
-        contadorBox.innerText = `${questoesDisponiveis.length+1}/${11}`//questões atuais com o máximo
-        pontosBox.innerHTML = `Pontuação: ${pontuacao}`; //pontuação na tela
-    }
-
-    CarregarProximaQuestao();//carrega nova pergunta aleatória 
+    CarregarProximaQuestao();
 });
